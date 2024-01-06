d1169355da2d75e9c0e1f4b0d17704605ae94459
Arnob <farhanasfar21@gmail.com>
2024-01-07 05:47:14 +0600

    Add testVarLet function to show difference between 'var' and 'let'

--- JS_Foundation/jsVariable.js
+++ JS_Foundation/jsVariable.js
@@ -5,3 +5,29 @@ var c = 'copilot';
 console.log(a+b+c); //output: 25copilot.
 
 console.log(typeof a, typeof b, typeof c); // would pring the data type.
+
+
+const value = 200; //const value can't be changed, not allowed.
+
+function testVarLet(){
+    console.log(x); //undefined
+    //console.log(y); //ReferenceError
+
+    var x = 10;
+    let y = 20;
+
+
+    if(true){
+        var x = 40; //var redeclares and modifies the 'x'
+
+        let y = 50; //it creates a new 'y' only in this block
+
+        console.log(x); //40
+        console.log(y); //50
+    }
+
+    console.log(x); //40
+    console.log(y); //20
+}
+
+testVarLet();
