2bcfbc12eca54a20cbd0d10ee0b615f0c59f7579
Arnob <farhanasfar21@gmail.com>
2024-01-30 19:31:02 +0600

    More 'object' practice

--- JS_Foundation/Object.js
+++ JS_Foundation/Object.js
@@ -1,8 +1,8 @@
 //common to use 'const' to declare objects if the variable is truly immutable.
 const student = {
-    "name": "Farhan Asfar",
-    "id": "2020-1-60-154",
-    "department": "CSE"
+    name: "Farhan Asfar",
+    id: "2020-1-60-154",
+    department: "CSE"
 }
 
 console.log(student);
@@ -13,9 +13,21 @@ console.log(student);
 
 
 const faculty = {
-    "name": "RDA",
-    "department": "CSE",
-    "salary": "120k"
+    name: "RDA",
+    department: "CSE",
+    salary: "120k"
 }
 
 console.log(faculty);
+
+
+const person = {
+    firstName : 'Farhan',
+    lastName : 'Asfar',
+    id : 154,
+    fullName: function(){
+        return this.firstName+ " "+ this.lastName; //'this' refers to the 'person' object
+    }
+};
+console.log(person)
+console.log(person.fullName());
