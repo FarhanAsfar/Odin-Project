1ddef4c36d3f583b6acb946b78f18e74e0d9558d
farhan <farhanasfar21@gmail.com>
2024-02-04 04:59:03 +0600

    Basic for-in loop

--- JS_Foundation/loop.js
+++ JS_Foundation/loop.js
@@ -3,3 +3,14 @@ console.log("hello");
 for (let i = 0; i < 10; i++) {
     console.log(i+1);   
 }
+
+let identity = {
+    name: 'farhan',
+    id: 154,
+    dept: 'cse'
+}
+
+for (const key in identity) { //for-in loop
+        const element = identity[key];
+        console.log(key+':'+element);
+}
