08e9e7b666efb7d938e42470a49ef09949fe7714
farhan <farhanasfar21@gmail.com>
2024-02-04 06:07:34 +0600

    Add array numbers using function

--- JS_Foundation/function.js
+++ JS_Foundation/function.js
@@ -4,3 +4,15 @@ function greet(name) {
 
 greet('world');
 greet('JavaScript');
+
+function arraySum(arr){
+    let sum = 0;
+    for(let i=0;i<arr.length;i++){
+        sum+=arr[i];
+    }
+    return sum;
+}
+let nums = [1,2,4,5];
+let result = arraySum(nums);
+console.log(result);
+
