8feeca2a2b1c738d520fcb49e18f8870d634ebf8
farhan <farhanasfar21@gmail.com>
2024-02-04 07:45:26 +0600

    Arrow function syntax

--- JS_Foundation/function.js
+++ JS_Foundation/function.js
@@ -16,3 +16,19 @@ let nums = [1,2,4,5];
 let result = arraySum(nums);
 console.log(result);
 
+
+//arrow function
+
+let sum = (a,b) =>a+b;
+//sum -> function name
+//(a,b) -> arguments
+//(a+b) -> expression
+console.log(sum(2,3));
+
+
+let age = 23;
+let welcome = (age<20)?
+() => console.log("Hello!"):
+() => console.log("Greetings!");
+
+welcome();
