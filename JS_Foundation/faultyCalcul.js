af59f16f01d412dad8a9960b7eb6a7ff4b591133
farhan <farhanasfar21@gmail.com>
2024-02-06 03:42:31 +0600

    Write 'wrongOperation' func. and add rand number

--- JS_Foundation/faultyCalcul.js
+++ JS_Foundation/faultyCalcul.js
@@ -9,7 +9,24 @@ function rightOperation(a, b){
 
     console.log('+: '+plus+'\n'+'-: '+minus+'\n'+'*: '+multi+'\n'+'/: '+divide);
 }
+
+function wrongOperation(a, b){
+    let plus = a*b;
+    let minus = a/b;
+    let multi = a+b;
+    let divide = a-b;
+
+    console.log('+: '+plus+'\n'+'-: '+minus+'\n'+'*: '+multi+'\n'+'/: '+divide);
+}
+
+
 let a = 10;
 let b = 5;
+let rand = Math.random();
+//console.log(rand);
 
-rightOperation(a,b);
+if(rand>0.3){
+    rightOperation(a,b);
+}else{
+    wrongOperation(a,b);
+}
