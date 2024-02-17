df41377660416317b50a92656b3dde652065e37a
Arnob <farhanasfar21@gmail.com>
2024-02-17 11:01:08 +0600

    Create new div and add different elements inside it

--- DOM/index.js
+++ DOM/index.js
@@ -1,15 +1,13 @@
 const container = document.querySelector('#container');
-
 const content = document.createElement('div');
 content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
-
 container.appendChild(content);
+
 
 const para = document.createElement('p');
 para.classList.add('red');
 para.textContent = 'This para is written using DOM!';
-
 container.appendChild(para);
 
 const redPara = document.querySelector('.red');
@@ -23,3 +21,16 @@ head.textContent = `I'm a blue h3`;
 container.appendChild(head);
 const hBlue = document.querySelector('.heading');
 hBlue.style.color = 'blue';
+
+
+
+const newDiv = document.createElement('div');
+newDiv.classList.add('elements');
+const hDiv = document.createElement('h1');
+const pDiv = document.createElement('p');
+hDiv.textContent = `I'm in a div`;
+pDiv.textContent = "ME TOO!";
+newDiv.appendChild(hDiv);
+newDiv.appendChild(pDiv);
+
+container.appendChild(newDiv);
