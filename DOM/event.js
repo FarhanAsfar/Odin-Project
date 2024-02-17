11ee1a1a3916a21e9a50a5c7d86727870c6bb951
Arnob <farhanasfar21@gmail.com>
2024-02-17 14:14:24 +0600

    Using function

--- DOM/event.js
+++ DOM/event.js
@@ -1,7 +1,11 @@
 const btn = document.querySelector('#btn');
 //btn.onclick = () => alert('Hello! Learning DOM!'); //arrow function. Method 2.
 
+function alertFunction(){
+    alert("Keep learning!");
+}
+btn.addEventListener('click', alertFunction);
 
-btn.addEventListener('click', () => {
-    alert("Hello World!");
-});
+// btn.addEventListener('click', () => {
+//     alert("Hello World!");
+// });
