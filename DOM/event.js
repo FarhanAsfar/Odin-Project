25c6ead6327f1c2159e8a9f6a4f3bcc395c5d085
Arnob <farhanasfar21@gmail.com>
2024-02-17 14:29:24 +0600

    Add listeners to group of nodes

--- DOM/event.js
+++ DOM/event.js
@@ -4,8 +4,22 @@ const btn = document.querySelector('#btn');
 function alertFunction(){
     alert("Keep learning!");
 }
-btn.addEventListener('click', alertFunction);
+btn.addEventListener('click', alertFunction); //try using function.
 
 // btn.addEventListener('click', () => {
 //     alert("Hello World!");
 // });
+
+// btn.addEventListener('click', function(e){
+//     e.target.style.background = 'blue'
+// });
+
+
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        alert('Button '+button.id+' is pressed');
+    });
+});
