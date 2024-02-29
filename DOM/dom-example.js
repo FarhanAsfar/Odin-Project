6d03df1045cd3df4bb2682d379e37723b12e74ac
Arnob <farhanasfar21@gmail.com>
2024-03-01 02:31:16 +0600

    create & append element

--- DOM/dom-example.js
+++ DOM/dom-example.js
@@ -1,11 +1,14 @@
+const link = document.querySelector('a');
+
+link.textContent = "Mozilla Developer Network";
+link.href = "https://developer.mozilla.org"; //changing the url link
+
 const section = document.querySelector('section');
 const content = document.createElement('a'); //adding new anchor tag.
 content.textContent = "JavaScript";
 content.href = "https://javascript.info";
 section.appendChild(content);
 
-
-const link = document.querySelector('a');
-
-link.textContent = "Mozilla Developer Network";
-link.href = "https://developer.mozilla.org"; //changing the url link
+const para = document.createElement('p');
+para.textContent = "MDN web docs";
+section.appendChild(para);
