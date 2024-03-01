d8bc55e7bd98afab1ad60a62728223b41ce1af37
Arnob <farhanasfar21@gmail.com>
2024-03-01 17:51:41 +0600

    change

--- DOM/dom-example.js
+++ DOM/dom-example.js
@@ -1,14 +1,19 @@
 const link = document.querySelector('a');
 
 link.textContent = "Mozilla Developer Network";
-link.href = "https://developer.mozilla.org"; //changing the url link
+link.href = "https://developer.mozilla.org"; //change the url link
 
 const section = document.querySelector('section');
-const content = document.createElement('a'); //adding new anchor tag.
+const content = document.createElement('a'); //add new anchor tag.
 content.textContent = "JavaScript";
 content.href = "https://javascript.info";
 section.appendChild(content);
 
-const para = document.createElement('p');
+const para = document.createElement('p'); //create new paragraph
 para.textContent = "MDN web docs";
 section.appendChild(para);
+
+
+const text = document.createTextNode("web dev knowledge");
+const linkPara = document.querySelector('p');
+linkPara.appendChild(text);
