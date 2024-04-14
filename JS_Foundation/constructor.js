9e340e1d00ce5e6c58696422ade23ec6c8e715c2
Arnob <farhanasfar21@gmail.com>
2024-04-14 19:49:29 +0600

    Use constructor

--- JS_Foundation/constructor.js
+++ JS_Foundation/constructor.js
@@ -1,8 +1,20 @@
 function createStudent(name){
     const student = {};
     student.name = name;
-    student.introduceSelf(){
+    student.introduceSelf = function() {
         console.log(`Hi! I'm ${this.name}.`);
     };
     return student;
 }
+
+const john = createStudent("John");
+john.introduceSelf();
+
+const bob = createStudent("Bob");
+bob.introduceSelf();
+
+
+//using constructor
+
+const alice = new createStudent("Alice");
+alice.introduceSelf();
