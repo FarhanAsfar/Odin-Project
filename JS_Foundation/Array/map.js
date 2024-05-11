e3386061fa371eb269868ac312a0b1450b56e0bf
Arnob <farhanasfar21@gmail.com>
2024-05-11 16:31:05 +0600

    filter method practice

--- JS_Foundation/Array/map.js
+++ JS_Foundation/Array/map.js
@@ -2,9 +2,16 @@ function addOne(num){
     return num+1;
 }
 
+function isOdd(num){
+    return num%2 !==0;
+}
+
 const arr = [1,2,3,4,5];
 const mappedArr = arr.map(addOne);
+
+const oddNum = arr.filter(isOdd);
 
 //map returns a new array and does not change the original array.
 console.log(arr);
 console.log(mappedArr);
+console.log(oddNum);
