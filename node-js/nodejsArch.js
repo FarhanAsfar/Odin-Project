e75a1f434d678ac270aa4db53ac665a0a30d7a1e
Arnob <farhanasfar21@gmail.com>
2024-06-12 02:01:31 +0600

    Sync/Async request

--- node-js/nodejsArch.js
+++ node-js/nodejsArch.js
@@ -1,7 +1,22 @@
 const fs = require("fs");
 
-//Synchronous---> BLOCKING REQUEST
-fs.writeFileSync('./test.txt', 'Hello node js server');
+// //Synchronous---> BLOCKING REQUEST
+// fs.writeFileSync('./test.txt', 'Hello node js server');
 
-//Asynchronous---> NON-BLOCKING REQUEST
-fs.writeFile('./test.txt', 'Hello Async node js', (err)=>{});
+// //Asynchronous---> NON-BLOCKING REQUEST
+// fs.writeFile('./test.txt', 'Hello Async node js', (err)=>{});
+
+
+//BLOCKING Request
+// console.log('1');
+// const result = fs.readFileSync("contact.txt", "utf-8");
+// console.log(result);
+// console.log('2');
+
+
+//Non-Blocking Request
+console.log('1');
+fs.readFile("contact.txt", "utf-8", (err, result)=>{
+    console.log(result);
+});
+console.log('2'); //2 will be printed after 1 and before the file contents.
