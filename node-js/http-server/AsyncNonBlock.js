a8e0aa0897e79db4135b2841297bfa6be710d5e2
Arnob <farhanasfar21@gmail.com>
2024-06-14 02:25:09 +0600

    NonBlocking operation using async/await

--- node-js/http-server/AsyncNonBlock.js
+++ node-js/http-server/AsyncNonBlock.js
@@ -1,1 +1,19 @@
 //Non-Blocking Ooperations with Promises and Async/Await
+
+const fs = require('fs').promises;
+
+console.log("start reading the file...");
+
+//Non-Blocking Operations using Async/Await
+async function readFile(){
+    try{
+        const data = await fs.readFile('./log.txt', 'utf-8');
+        console.log(data);
+    }catch(err){
+        console.error(err);
+    }
+}
+
+readFile();
+
+console.log("This line is written at the end but will be printed before the file contents");
