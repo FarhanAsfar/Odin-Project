c23d40bc91c2f2c8adbd68ecde3b7298c12d3262
Arnob <farhanasfar21@gmail.com>
2024-06-14 02:03:08 +0600

    Blocking operation

--- node-js/http-server/practice1.js
+++ node-js/http-server/practice1.js
@@ -2,7 +2,14 @@ const fs = require('fs');
 
 console.log("start reading the content of the file...");
 
-const data = fs.readFile('./log.txt', 'utf-8', (err,res)=>{
+//Blocking Operations..
+const data = fs.readFileSync('./log.txt', 'utf-8');
+console.log(data);
+
+
+
+//Non-Blocking Operations..
+fs.readFile('./log.txt', 'utf-8', (err,res)=>{
     if(err){
         console.error(err);
     }else{
