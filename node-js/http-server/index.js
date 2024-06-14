d80418c974d437aca0f1fb0c3ea99b6850159c8d
Arnob <farhanasfar21@gmail.com>
2024-06-14 19:32:42 +0600

    Response for different pages

--- node-js/http-server/index.js
+++ node-js/http-server/index.js
@@ -7,7 +7,17 @@ const myServer = http.createServer((req, res) => {
     //console.log(req.headers);
     const log = `${Date.now()}:New req. received\n`;
     fs.appendFile("log.txt",log, (err,data)=>{
-        res.end("hello from server");    
+        //res.end("hello from server");    
+        switch (req.url){
+            case "/":
+                res.end("Homepage");
+                break;
+            case "/about":
+                res.end("About Page");
+                break;
+            default:
+                res.end("404 Not Found!");
+        }
     })
     
 });
