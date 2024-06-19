1fb97f82bf51d91d37b1bf9bdb1bdaa72a5b3937
Arnob <farhanasfar21@gmail.com>
2024-06-19 18:34:38 +0600

    Create sum function for server

--- express/index.js
+++ express/index.js
@@ -4,8 +4,18 @@ const express = require("express");
 
 const app = express();
 
+function sum(n){
+    let ans = 0;
+    for(let i = 1; i<=n; i++){
+        ans = ans+i;
+    }
+    return ans;
+}
+
 app.get("/", function(req, res){
-    res.send("hello from server");
+    const n =req.query.n;
+    const ans = sum(n);
+    res.send("hello from server "+ans);
 })
 
 app.listen(3000);
