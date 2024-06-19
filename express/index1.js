3b70c968cdf1e67e8d1f929b96640debc6e0376a
Arnob <farhanasfar21@gmail.com>
2024-06-20 01:43:42 +0600

    Implement DELETE method

--- express/index1.js
+++ express/index1.js
@@ -50,4 +50,17 @@ app.put("/", function(req,res){
     res.json({});
 })
 
+app.delete("/", function(req, res){
+    const newKidney = [];
+    for(let i=0; i<users[0].kidney.length; i++){
+        if(users[0].kidney[i].healthy){
+            newKidney.push({
+                healthy: true
+            })
+        }
+    }
+    users[0].kidney = newKidney;
+    res.json({msg:"Done..!"})
+})
+
 app.listen(3000);
