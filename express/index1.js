5be03335aa27a4c92626d2b1ea1a2dad5e290d95
Arnob <farhanasfar21@gmail.com>
2024-06-20 00:53:16 +0600

    Implement app.get

--- express/index1.js
+++ express/index1.js
@@ -5,12 +5,28 @@ const app = express();
 const users = [{
     name: "Alice",
     kidney: [{
-        healthy: false
+        healthy: false,
     }],
 }];
 
 app.get("/", function(req, res){
     const aliceKidney = users[0].kidney;
+    const kidneyNo = aliceKidney.length; 
+    let healthyKidney = 0;
+
+    for(let i=0; i<aliceKidney.length; i++){
+        if(aliceKidney[i].healthy){
+            kidneyNo = healthyKidney+1;
+        }
+    }
+    const unhealthyKidney = kidneyNo - healthyKidney;
+
+    res.json({
+        aliceKidney,
+        healthyKidney,
+        unhealthyKidney
+    })
+
     console.log(aliceKidney);
 });
 
