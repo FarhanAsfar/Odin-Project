d70a3b46c610371d9157be99598c887311ef7efa
Arnob <farhanasfar21@gmail.com>
2024-06-19 18:30:27 +0600

    Create server

--- express/index.js
+++ express/index.js
@@ -1,1 +1,11 @@
 //creating a http server using express
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", function(req, res){
+    res.send("hello from server");
+})
+
+app.listen(3000);
