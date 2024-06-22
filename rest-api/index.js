a7d9ff051f2ff2611f212fed812548e2078b4844
Arnob <farhanasfar21@gmail.com>
2024-06-22 17:13:28 +0600

    Task1:list all users-done

--- rest-api/index.js
+++ rest-api/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const users = require("./MOCK_DATA.json");
 
 const app = express();
 const port = 3000;
+
+
+app.get('/users', function(req,res){
+    return res.json(users);
+})
 
 app. listen(port, ()=> console.log(`PORT:${port} has started...`));
