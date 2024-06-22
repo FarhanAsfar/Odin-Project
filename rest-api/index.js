9870f5d4580e52839498311a2d4d7194bbf9e403
Arnob <farhanasfar21@gmail.com>
2024-06-22 17:30:07 +0600

    Task2: get users with id

--- rest-api/index.js
+++ rest-api/index.js
@@ -4,11 +4,8 @@ const users = require("./MOCK_DATA.json");
 const app = express();
 const port = 3000;
 
-
-app.get('/api/users', function(req,res){
-    return res.json(users);
-});
-
+//Routes
+//Render html document
 app.get('/users', function(req,res){
     const html = `
     <ul>
@@ -17,5 +14,20 @@ app.get('/users', function(req,res){
     `; 
     res.send(html);
 });
+
+// REST API
+app.get('/api/users', function(req,res){
+    return res.json(users);
+});
+
+app.get('/api/users/:id', function(req,res){
+    const id = Number(req.params.id);
+    const user = users.find(user => user.id === id);
+    return res.json(user);
+})
+
+
+
+
 
 app. listen(port, ()=> console.log(`PORT:${port} has started...`));
