59e4417c94b232827402d2d0daaafba67023ce46
Arnob <farhanasfar21@gmail.com>
2024-06-22 17:24:46 +0600

    Render HTML document

--- rest-api/index.js
+++ rest-api/index.js
@@ -5,8 +5,17 @@ const app = express();
 const port = 3000;
 
 
-app.get('/users', function(req,res){
+app.get('/api/users', function(req,res){
     return res.json(users);
-})
+});
+
+app.get('/users', function(req,res){
+    const html = `
+    <ul>
+        ${users.map((user) => `<li>${user.first_name}</li>`)}
+    </ul>
+    `; 
+    res.send(html);
+});
 
 app. listen(port, ()=> console.log(`PORT:${port} has started...`));
