ad71a633da9831fc86a43566fdf155b72e3900d4
Arnob <farhanasfar21@gmail.com>
2024-06-24 02:30:55 +0600

    Arrow function practice

---
+++ JS_Foundation/Array/arrow.js
@@ -0,0 +1,13 @@
+//arrow function
+
+function sum(a, b){ //regular way of writing function
+    return a+b;
+}
+
+const summ = (a,b)=>{ //arrow function
+    return a+b;
+}
+
+console.log(sum(2,3));
+
+console.log(summ(1,2));
