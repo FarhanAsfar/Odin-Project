0ae111d732153ce03bc4779cb9976de9a094d413
arnob <farhanasfar21@gmail.com>
2024-07-20 22:43:52 +0600

    fs.readFile & setTimeout

--- JS_Foundation/Callback_Async_Promises/async.js
+++ JS_Foundation/Callback_Async_Promises/async.js
@@ -5,19 +5,37 @@
 // setTimeout(onDone, 2000);
 // console.log("after calling onDone");
 
-function findSum(){
+function findSum(n){
   let ans = 0;
-  for(let i=0;i<10;i++){
+  for(let i=0;i<n;i++){
     ans+=i;
   }
   return ans;
 }
 
 function findSumTill100(){
-  console.log(findSum(10));
+  console.log(findSum(20));
 }
 
 setTimeout(findSumTill100, 1000);
 
 console.log("hello");
+
+//-----------------------------------------------------------------------//
+
+console.log("hi");
+
+setTimeout(()=>{
+  console.log("async 1")
+}, 20000);
+
+setTimeout(()=>{
+  console.log("async 2")
+}, 10000);
+
+let a=0;
+for(let i=0; i<10; i++){
+  a+=i;
+}
+console.log(a);
 
--- JS_Foundation/Callback_Async_Promises/readFile_async.js
+++ JS_Foundation/Callback_Async_Promises/readFile_async.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 
 fs.readFile("a.txt", "utf8", function(err, data){
+  if(err){
+    console.log("Invalid File");
+  }
   console.log(data);
 });
 
 console.log("after reading file");//will execute first
+
