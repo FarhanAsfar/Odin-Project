3995a249b262b72838282f4c2d39853392f274a2
arnob <farhanasfar21@gmail.com>
2024-07-20 22:25:08 +0600

    setTimeout

--- JS_Foundation/Callback_Async_Promises/async.js
+++ JS_Foundation/Callback_Async_Promises/async.js
@@ -1,6 +1,23 @@
-function onDone() {
-  console.log("hello");
+// function onDone() {
+//   console.log("hello");
+// }
+
+// setTimeout(onDone, 2000);
+// console.log("after calling onDone");
+
+function findSum(){
+  let ans = 0;
+  for(let i=0;i<10;i++){
+    ans+=i;
+  }
+  return ans;
 }
 
-setTimeout(onDone, 2000);
-console.log("after calling onDone");
+function findSumTill100(){
+  console.log(findSum(10));
+}
+
+setTimeout(findSumTill100, 1000);
+
+console.log("hello");
+
