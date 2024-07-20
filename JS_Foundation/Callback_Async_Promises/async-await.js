dea000a7b35d8f7005ca13c1437f2840de04c3ba
arnob <farhanasfar21@gmail.com>
2024-07-21 02:26:17 +0600

    Callback, Promise, Async-Await comparison

--- JS_Foundation/Callback_Async_Promises/async-await.js
+++ JS_Foundation/Callback_Async_Promises/async-await.js
@@ -1,3 +1,37 @@
+//----------------Callback Syntax--------------------------//
+function myAsycnFunction(callback) {
+    cancelIdleCallback("hello");
+  }
+  
+  function main() {
+      myAsycnFunction(function(value){
+        console.log(value);
+      }); 
+  }
+  
+  main();
+//-----------------------------------------------------------------//
+
+
+//----------------Promise Syntax-----------------------------------//
+
+function myAsycnFunction() {
+    let p = new Promise(function (resolve) {
+      resolve("hello");
+    });
+    return p;
+  }
+  
+  function main() {
+      myAsycnFunction().then(function(value){
+        console.log(value);
+      }); 
+  }
+  main();
+//------------------------------------------------------------------------//
+
+
+//-------------------------Async-Await Syntax-----------------------------//
 function myAsycnFunction() {
   let p = new Promise(function (resolve) {
     resolve("hello");
@@ -9,6 +43,5 @@ async function main() {
     let value = await myAsycnFunction(); //no callback syntax, no '.then' syntax 
     console.log(value);
 }
-
 
 main();
