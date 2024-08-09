453e63ad80c9ccdc8e8ade3869a5304b785dbdc1
arnob <farhanasfar21@gmail.com>
2024-08-10 04:19:10 +0600

    Validating object using zod

--- authentication/zod/index.js
+++ authentication/zod/index.js
@@ -2,13 +2,20 @@ const express = require('express');
 const zod = require('zod');
 const app = express();
 
-const schema = zod.array(zod.number());
+// const schema = zod.array(zod.number());
+
+const schema = zod.object({
+    email: zod.string(),
+    password: zod.string(),
+    country: zod.literal("BAN").or(zod.literal("CAN")),
+    kidney: zod.array(zod.number())
+});
 
 app.use(express.json());
 
 app.post('/health', (req, res)=>{
     const kidney = req.body.kidney;
-    const response = schema.safeParse(kidney);
+    const response = schema.safeParse(req.body);
 
     if(!response.success){
         res.status(411).json({
