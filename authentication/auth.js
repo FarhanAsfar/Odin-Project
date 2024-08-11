d01e3847d69ee7d8a5f5f06fd81ff1dfaed9616d
arnob <farhanasfar21@gmail.com>
2024-08-11 20:32:34 +0600

    try catch

--- authentication/auth.js
+++ authentication/auth.js
@@ -14,4 +14,4 @@ console.log(token);
 
 const verifyToken = jwt.verify("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiRmFyaGFuIiwiYWNjb3VudE51bWJlciI6MTIzNCwiaWF0IjoxNzIzMzg1NzUwfQ.U_Rsza_0HclqddCJsCpBvHT6kZkdMGFXuCv7EW85xQM", "secret");
 
-console.log(verifyToken);
+console.log(verifyToken); 
---
+++ authentication/trycatch.js
@@ -0,0 +1,9 @@
+try{
+    let b;
+    let a = [1,2,2];
+    console.log(a.length);
+    console.log(b.length);
+}catch(e){
+    console.log("inside catch");
+}
+console.log("hi")
