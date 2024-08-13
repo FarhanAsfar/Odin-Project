15403a36c6b9a31d38f43deaf00a3f924a4a7423
arnob <farhanasfar21@gmail.com>
2024-08-13 07:06:22 +0600

    MongoDB: connect a route with mongoDB

--- database/index.js
+++ database/index.js
@@ -1,52 +1,36 @@
-const express = require("express");
-const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
-const jwtPassword = "123456";
-
-mongoose.connect(
-  "your_mongo_url",
-);
-
-const User = mongoose.model("User", {
-  name: String,
-  username: String,
-  pasword: String,
-});
-
+const express = require("express");
 const app = express();
+
 app.use(express.json());
 
-function userExists(username, password) {
-  // should check in the database
-}
+mongoose.connect(
+  "mongodb+srv://farhan:farhan21@cluster0.0qnn6.mongodb.net/user_app",
+);
 
-app.post("/signin", async function (req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  if (!userExists(username, password)) {
-    return res.status(403).json({
-      msg: "User doesnt exist in our in memory db",
-    });
-  }
-
-  var token = jwt.sign({ username: username }, "shhhhh");
-  return res.json({
-    token,
-  });
+const User = mongoose.model("Users", {
+  name: String,
+  email: String,
+  password: String,
 });
 
-app.get("/users", function (req, res) {
-  const token = req.headers.authorization;
-  try {
-    const decoded = jwt.verify(token, jwtPassword);
-    const username = decoded.username;
-    // return a list of users other than this username from the database
-  } catch (err) {
-    return res.status(403).json({
-      msg: "Invalid token",
-    });
+app.post("/signup", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  const existUser = await User.findOne({ email: email });
+  if (existUser) {
+    return res.status(400).send("User already exist");
   }
+
+  const user = new User({
+    name: username,
+    email: email,
+    password: password,
+  });
+
+  await user.save();
+
+  res.json({ message: "User created successfully" });
 });
 
 app.listen(3000);
